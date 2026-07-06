ffb1e55229ed0c6fef278bff50d218e31aba85a8
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:12:25 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,16 @@ output "dev_center_environment_types" {
   description = "All dev_center_environment_type resources"
   value       = azurerm_dev_center_environment_type.dev_center_environment_types
 }
+output "dev_center_environment_types_dev_center_id" {
+  description = "List of dev_center_id values across all dev_center_environment_types"
+  value       = [for k, v in azurerm_dev_center_environment_type.dev_center_environment_types : v.dev_center_id]
+}
+output "dev_center_environment_types_name" {
+  description = "List of name values across all dev_center_environment_types"
+  value       = [for k, v in azurerm_dev_center_environment_type.dev_center_environment_types : v.name]
+}
+output "dev_center_environment_types_tags" {
+  description = "List of tags values across all dev_center_environment_types"
+  value       = [for k, v in azurerm_dev_center_environment_type.dev_center_environment_types : v.tags]
+}
 
