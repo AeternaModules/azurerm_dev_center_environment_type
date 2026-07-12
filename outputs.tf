322c5725c86e0b0255dcff80f19a4c981b7d3374
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:14 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "dev_center_environment_types" {
-  description = "All dev_center_environment_type resources"
-  value       = azurerm_dev_center_environment_type.dev_center_environment_types
-}
 output "dev_center_environment_types_dev_center_id" {
-  description = "List of dev_center_id values across all dev_center_environment_types"
-  value       = [for k, v in azurerm_dev_center_environment_type.dev_center_environment_types : v.dev_center_id]
+  description = "Map of dev_center_id values across all dev_center_environment_types, keyed the same as var.dev_center_environment_types"
+  value       = { for k, v in azurerm_dev_center_environment_type.dev_center_environment_types : k => v.dev_center_id }
 }
 output "dev_center_environment_types_name" {
-  description = "List of name values across all dev_center_environment_types"
-  value       = [for k, v in azurerm_dev_center_environment_type.dev_center_environment_types : v.name]
+  description = "Map of name values across all dev_center_environment_types, keyed the same as var.dev_center_environment_types"
+  value       = { for k, v in azurerm_dev_center_environment_type.dev_center_environment_types : k => v.name }
 }
 output "dev_center_environment_types_tags" {
-  description = "List of tags values across all dev_center_environment_types"
-  value       = [for k, v in azurerm_dev_center_environment_type.dev_center_environment_types : v.tags]
+  description = "Map of tags values across all dev_center_environment_types, keyed the same as var.dev_center_environment_types"
+  value       = { for k, v in azurerm_dev_center_environment_type.dev_center_environment_types : k => v.tags }
 }
 
